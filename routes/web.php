4afb728df3baa1e721a80b512bf6fe80dcff2baf
JunHatanaka <nextseason6@gmail.com>
2025-06-25 02:51:10 +0900

    feat(route): routeのサンプル実装

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome'); // テキストをそのまま表示
+    return '<html><body><h1>Welcome to My Laravel Application</h1></body></html>'; // HTMLを直接返す
+});
+
+Route::get('/hello/{msg}', function ($msg) { // パラメータを受け取るルート
+    return "<html><body><h1>About Us</h1><p>parameter is 「{$msg}」</p></body></html>"; // パラメータを含むHTMLを返す
 });
